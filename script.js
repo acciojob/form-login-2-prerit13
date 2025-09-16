02a4bde3474e6d47eed024bd89bc617f5c3fa29e
prerit13 <147128014+prerit13@users.noreply.github.com>
2025-09-17 00:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ document.getElementById('userForm').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent actual form submission
+
+            const firstName = document.getElementById('firstName').value;
+            const lastName = document.getElementById('lastName').value;
+            const phoneNumber = document.getElementById('phoneNumber').value;
+            const emailId = document.getElementById('emailId').value;
+
+            const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+            alert(message);
+        });
